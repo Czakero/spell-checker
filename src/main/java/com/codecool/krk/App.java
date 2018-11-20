231c6d9c246521f01824950f0e19193024065a2d
Czakero <bartosz.pyrz96@gmail.com>
2018-11-20 22:37:18 +0100

    App for testing

--- src/main/java/com/codecool/krk/App.java
+++ src/main/java/com/codecool/krk/App.java
@@ -1,13 +1,16 @@
 package com.codecool.krk;
 
+import java.io.File;
+
 /**
  * Hello world!
- *
+ *k
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        WordList wordList = new WordList(new File("src/resources/wordlist.txt"));
+        WordChecker wordChecker = new WordChecker(wordList);
+        System.out.println(wordChecker.getSuggestions("lne"));
     }
 }
